8140261abe20356236de75c8391b62d06bd6e6a1
Amarendra Kumar <maiamarendra.ti@gmail.com>
2020-04-10 23:18:40 +0530

    n-banoci program

--- NBonacciNumbers.java
+++ NBonacciNumbers.java
@@ -5,16 +5,19 @@ public class NBonacciNumbers {
 	public static void main(String[] args) {
 		int n=3;//like 3 bonaki 
 		int m=8;//print number till m count
-		if(m<n) {
+		if(m<n || n<3) {
 			System.out.println("Need to throw exception");
 		}
-		int a1=0; 
-		int a2=0; 
-		int a3=1;
-		for(int i=0;i<m;i++) {
-			
+		int a[]= new int[m];
+		for(int i =0;i<m;i++)
+			a[i]=0;
+		a[n-1]=1;
+		a[n]=1;
+		for(int i=n+1;i<m;i++) {
+			a[i]= a[i-1]+a[i-1]-a[i-n-1];
 		}
-		
+		for(int i =0;i<m;i++)
+			System.out.print(a[i]+" ");
 
 	}
 
